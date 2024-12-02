af5d8a30232bf270470f168a2c0522159506204d
Adam Gajda <80600850+agajdac@users.noreply.github.com>
2024-12-02 18:08:37 +0100

    Day 02

---
+++ 02_main.ml
@@ -0,0 +1,26 @@
+open Base
+open Stdio
+
+let is_safe ?(remove_index = Int.max_value) line =
+  let line = String.split_on_chars line ~on:[ ' ' ] in
+  let line = List.map line ~f:Int.of_string in
+  let line = List.filteri line ~f:(fun i _ -> i <> remove_index) in
+  fst
+  @@ List.fold line
+       ~init:(true, List.nth_exn line 0 - 1)
+       ~f:(fun (acc, prev) curr ->
+         if Int.abs (curr - prev) > 3 then (false, curr) else (acc, curr))
+  && (List.is_sorted_strictly line ~compare:Int.descending
+     || List.is_sorted_strictly line ~compare:Int.ascending)
+
+let solve lines = List.count lines ~f:is_safe
+
+let solve_2 lines =
+  List.count lines ~f:(fun line ->
+      List.existsi lines ~f:(fun i _ -> is_safe ~remove_index:i line))
+
+let () =
+  let content = In_channel.read_all "data.txt" in
+  let lines = String.split_lines content in
+  solve lines |> printf "%d\n";
+  solve_2 lines |> printf "%d\n"
